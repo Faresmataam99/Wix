1e1cb4500567aa1bfa5538b890d959b9dd73a35c
Zait Fares <fares.plackfurt12@gmail.com>
2024-09-25 19:58:31 +0100

    wix

---
+++ script.js
@@ -0,0 +1,28 @@
+alert("enjoy the recepie");
+let firstname = "Steve"
+console.log(firstname)
+let lastname ="Zait"
+console.log(lastname)
+let age ="22"
+console.log(age)
+let adress ="London"
+console.log(adress)
+let oldage= 80;
+console.log(oldage)
+oldage = 50
+console.log(oldage)
+
+let year=2024
+let myage=22
+let birthyear= year-myage
+console.log(birthyear)
+console.log(firstname+" "+lastname)
+let yourage= prompt("write your age")
+console.log(yourage)
+let currentage=20
+let futureage=(currentage + 10)
+console.log(futureage)
+
+
+
+
